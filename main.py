ba6c9e4b901a16e7c17f787333ba79d7779124c4
kizons <38180110+kizons@users.noreply.github.com>
2022-07-12 01:16:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color:\n 'red', "
+                                                          "'yellow', 'blue', 'green', 'brown'")
+colors = ["red", "yellow", "blue", "green", "brown"]
+y_positions = [-100, -60, -20, 20, 60]
+all_turtles = []
+
+for turtle_index in range(0, 5):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+
+screen.exitonclick()
+
+# def move_forwards():
+#     tim.forward(10)
+#
+#
+# def move_backwards():
+#     tim.backward(10)
+#
+#
+# def turn_left():
+#     new_heading = tim.heading() + 10
+#     tim.setheading(new_heading)
+#
+#
+# def turn_right():
+#     new_heading = tim.heading() - 10
+#     tim.setheading(new_heading)
+#
+#
+# def clear_screen():
+#     tim.clear()
+#     tim.penup()
+#     tim.home()
+#     tim.pendown()
+#
+#
+# screen.listen()
+# screen.onkey(move_forwards, "W")
+# screen.onkey(move_backwards, "S")
+# screen.onkey(turn_left, "A")
+# screen.onkey(turn_right, "D")
+# screen.onkey(clear_screen, "C")
+
+
+# screen.exitonclick()
